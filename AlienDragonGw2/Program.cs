5205245c2827f661bb0f537d6135162cafb96fc5
seb <seb@seb.fr>
2014-02-12 00:23:23 +0100

    Fichier de Configuration

--- AlienDragonGw2/Program.cs
+++ AlienDragonGw2/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Net;
 using System.Threading.Tasks;
 using LightFX;
 
@@ -12,60 +14,33 @@ namespace AlienDragonGw2
         static void Main(string[] args)
         {
 
+            var json = new WebClient().DownloadString("https://api.guildwars2.com/v1/events.json?world_id=1001&map_id=39");
+
+            string ColorShatterer = ConfigurationManager.AppSettings["ColorShatterer"];
+            string ColorJormag = ConfigurationManager.AppSettings["ColorJormag"];
+            Console.WriteLine("Shatterer {0} ", ColorShatterer);
+            Console.WriteLine("Jormag {0} ", ColorJormag);
+            
+            //Console.WriteLine(json);
             var lightFX = new LightFXController();
 
             var result = lightFX.LFX_Initialize();
             if (result == LFX_Result.LFX_Success)
             {
+
                 lightFX.LFX_Reset();
 
-                uint numDevs;
-                lightFX.LFX_GetNumDevices(out numDevs);
-
-                for (uint devIndex = 0; devIndex < numDevs; devIndex++)
-                {
-                    uint numLights;
-                    lightFX.LFX_GetNumLights(devIndex, out numLights);
-
-                    var green = new LFX_ColorStruct(255, 0, 255, 0);
-                    var red = new LFX_ColorStruct(255, 255, 0, 0);
-                    for (uint lightIndex = 0; lightIndex < numLights; lightIndex++)
-                        lightFX.LFX_SetLightColor(devIndex, lightIndex, lightIndex % 2 == 0 ? red : green);
-                }
-
-
-                for (uint devIndex = 0; devIndex < numDevs; devIndex++)
-                {
-                    StringBuilder devDescription;
-                    LFX_DeviceType type;
-
-                    result = lightFX.LFX_GetDeviceDescription(devIndex, out devDescription, 255, out type);
-                    if (result != LFX_Result.LFX_Success)
-                        continue;
-
-                    Console.WriteLine(string.Format("Device: {0} \tDescription: {1} \tType: {2}", devIndex, devDescription, type));
-
-                    uint numLights;
-                    lightFX.LFX_GetNumLights(devIndex, out numLights);
-                    for (uint lightIndex = 0; lightIndex < numLights; lightIndex++)
-                    {
-                        StringBuilder description;
-                        result = lightFX.LFX_GetLightDescription(devIndex, lightIndex, out description, 255);
-                        if (result != LFX_Result.LFX_Success)
-                            continue;
-
-                        LFX_ColorStruct color;
-                        result = lightFX.LFX_GetLightColor(devIndex, lightIndex, out color);
-                        if (result != LFX_Result.LFX_Success)
-                            continue;
-
-                        Console.WriteLine(string.Format("\tLight: {0} \tDescription: {1} \tColor: {2}", lightIndex, description, color));
-                    }
-                }
-
+                // Light the keyboard
+                var primaryColor = new LFX_ColorStruct(255, 0, 255, 0);
+                lightFX.LFX_Light(LFX_Position.LFX_All, primaryColor);
                 lightFX.LFX_Update();
+
+
                 Console.WriteLine("Done.\r\rPress ENTER key to finish ...");
                 Console.ReadLine();
+
+
+                // When the program end , the original lighting configuration is restoreed
                 lightFX.LFX_Release();
             }
             else
@@ -81,8 +56,6 @@ namespace AlienDragonGw2
                 }
             }
 
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
--- readme.md
+++ readme.md
@@ -9,7 +9,11 @@ AlienDragonGw2 is a project for AlienWare laptop users. This application will li
 In order to launch the application , you need to have AlienWareFX running with lights activated.
 
 P.S : To get AlienWareFX running , you need to lunch AlienWare Command Center and put lights on
-## Exemple
+
+## Installation
+
+You don't have to install this software to use It. Just run the AlienDragonGw2.exe
+
 
 	Exemple
 
