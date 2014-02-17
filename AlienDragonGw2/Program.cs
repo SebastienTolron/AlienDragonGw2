36c2fcc72ccf00ba73e52f56719ec6993eac9944
seb <seb@seb.fr>
2014-02-17 10:44:58 +0100

    Test de multiple repo

--- AlienDragonGw2/Program.cs
+++ AlienDragonGw2/Program.cs
@@ -12,22 +12,42 @@ namespace AlienDragonGw2
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
 
-          
+
             // Get Data from gw2 APi
 
             // Event details https://api.guildwars2.com/v1/event_details.json?event_id=598CF3BD-0DF4-4FC7-97EA-AB4515497F5E&lang=fr
+            // Event Active or Inactive  https://api.guildwars2.com/v1/events.json?world_id=1001&event_id=03BF176A-D59F-49CA-A311-39FC6F533F2F
+
+            // Test :
+                 // Fort ranik ID : 2102
+                // Lang = fr 2102
 
             Console.WriteLine("Loading Gw2 datas....");
-            var json = new WebClient().DownloadString("https://api.guildwars2.com/v1/events.json?world_id=1001&map_id=39");
             // Print all events status
             Hashtable section = (Hashtable)ConfigurationManager.GetSection("DragonSettings/Id");
             foreach (DictionaryEntry d in section)
             {
-             Console.WriteLine("{0} ; {1}", d.Key, d.Value);
-           
+
+                string worldId = "2102";
+                string lang = "fr";
+                try
+                {
+                    var json = new WebClient().DownloadString("https://api.guildwars2.com/v1/events.json?world_id=" + worldId + "&event_id=" + d.Value + "&lang=" + lang);
+                    Console.WriteLine(json);
+                    // Console.WriteLine("{0} ; {1}", d.Key, statusEvent);
+                }
+                catch (Exception e)
+                    {
+                        
+                        Console.WriteLine("There was an error trying to get gw2 datas.");
+                        Console.WriteLine("Done.\r\rPress ENTER key to finish ...");
+                        Console.ReadLine();
+                        return 0;
+
+                    }
             }
 
 
@@ -57,6 +77,8 @@ namespace AlienDragonGw2
                 Console.WriteLine("Done.\r\rPress ENTER key to finish ...");
                 Console.ReadLine();
 
+                lightFX.LFX_Release();
+
 
                 // When the program end , the original lighting configuration is restoreed
 
@@ -69,13 +91,18 @@ namespace AlienDragonGw2
                 {
                     case LFX_Result.LFX_Error_NoDevs:
                         Console.WriteLine("There is not AlienFX device available.");
+                        Console.WriteLine("Done.\r\rPress ENTER key to finish ...");
+                        Console.ReadLine();
                         break;
                     default:
                         Console.WriteLine("There was an error initializing the AlienFX device.");
+                        Console.WriteLine("Done.\r\rPress ENTER key to finish ...");
+                        Console.ReadLine();
                         break;
                 }
             }
 
+            return 1;
         }
     }
 }
